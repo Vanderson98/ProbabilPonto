878264bb327c83b8f9d582f652f56ad4a75aa058
Vanderson Souza <84995202+Vanderson98@users.noreply.github.com>
2023-11-11 16:22:56 -0300

    Update
    Adicionando logica para criar caixas de inserção de nomes de acordo com o numero de jogadores selecionado, e adição de design visual disso

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,11 +15,32 @@ let playGame = ()=>{
 
 
 let players = (number) =>{
-    if(number == 2){
-        console.log('Dois jogadores');
-    }else if(number == 3){
-        console.log('Três jogadores');
-    }else {
-        console.log('Quatro jogadores');
+    containerFluid.innerHTML = ''; // Limpando todo o HTML
+    let boxNamesPlayers = document.createElement('div'); // Criando div
+    boxNamesPlayers.classList.add('boxNamesPlayeres') // Adicionando nome da classe
+
+    for(let i = 1; i <= number; i++){ // Criar box de acordo com o numero de jogadores
+        let boxName = document.createElement('div');
+        boxName.classList.add('boxName');
+
+        let titleName = document.createElement('h3');
+        titleName.classList.add('titleName');
+        titleName.innerHTML = `Jogador ${i}`;
+
+        let inputName = document.createElement('input');
+        inputName.classList.add('inputName');
+
+        boxNamesPlayers.appendChild(boxName); // Adicionando a div de boxNamesPlayers
+        boxName.appendChild(titleName); // Adicionando title
+        boxName.appendChild(inputName) // Adicionando input ao boxName
     }
+    
+    let buttonPlay = document.createElement('button');
+    buttonPlay.classList.add('btnToPlay');
+    buttonPlay.innerHTML = "<h3 class='titlePlayBtn'>Jogar</h3>";
+
+    containerFluid.appendChild(boxNamesPlayers) // Adicionar div no conteudo
+
+    containerFluid.appendChild(buttonPlay);
+
 }
